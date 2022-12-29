d13d51f53d24ff7c3c1208b99a440c7b90efd496
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2022-12-28 21:06:47 -0300

    fix: implements mandatory validate function

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,31 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
+type Payload = {
+  sub: number;
+  email: string;
+};
+
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'secret',
     });
+  }
+
+  public async validate({ sub }: Payload) {
+    const user = await this.userRepository.findOneBy({
+      id: sub,
+    });
+    const { hash, ...output } = user;
+    return output;
   }
 }
