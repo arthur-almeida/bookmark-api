579f10bf4f4a5f235610d9fa92d2d032a7dfad1e
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2023-01-02 15:47:38 -0300

    feat: implements logic to create a bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorators';
 import { JwtGuard } from 'src/auth/guards';
+import { User } from 'src/user/entities/user.entity';
 import { BookmarkService } from './bookmark.service';
+import { CreateBookmarkDto } from './dtos';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
@@ -11,5 +13,13 @@ export class BookmarkController {
   @Get()
   getBookmarks(@GetUser('id') userId: number) {
     return this.bookmarkService.getBookmarks(userId);
+  }
+
+  @Post()
+  createBookmark(
+    @GetUser() user: User,
+    @Body() createBookmarkDto: CreateBookmarkDto,
+  ) {
+    return this.bookmarkService.createBookmark(user, createBookmarkDto);
   }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateBookmarkDto } from './dtos';
 import { Bookmark } from './entities/bookmark.entity';
 
 @Injectable()
@@ -18,5 +20,13 @@ export class BookmarkService {
         },
       },
     });
+  }
+
+  public createBookmark(user: User, createBookmarkDto: CreateBookmarkDto) {
+    const bookmark = this.bookmarkRepository.create({
+      ...createBookmarkDto,
+      user,
+    });
+    return this.bookmarkRepository.save(bookmark);
   }
 }
