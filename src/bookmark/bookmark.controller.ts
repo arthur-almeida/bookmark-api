7ed5bf97341e637bad7775ad6f5c13d4a542e752
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2023-01-02 19:25:36 -0300

    feat: implements logic to update a bookmark

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { GetUser } from 'src/auth/decorators';
 import { JwtGuard } from 'src/auth/guards';
 import { User } from 'src/user/entities/user.entity';
 import { BookmarkService } from './bookmark.service';
-import { CreateBookmarkDto } from './dtos';
+import { CreateBookmarkDto, UpdateBookmarkDto } from './dtos';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
@@ -15,11 +24,32 @@ export class BookmarkController {
     return this.bookmarkService.getBookmarks(userId);
   }
 
+  @Get(':id')
+  getBookmarksById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
+
   @Post()
   createBookmark(
     @GetUser() user: User,
     @Body() createBookmarkDto: CreateBookmarkDto,
   ) {
     return this.bookmarkService.createBookmark(user, createBookmarkDto);
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() updateBookmarkDto: UpdateBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmarkById(
+      userId,
+      bookmarkId,
+      updateBookmarkDto,
+    );
   }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateBookmarkDto } from './dtos';
+import { CreateBookmarkDto, UpdateBookmarkDto } from './dtos';
 import { Bookmark } from './entities/bookmark.entity';
 
 @Injectable()
@@ -22,11 +22,46 @@ export class BookmarkService {
     });
   }
 
+  public getBookmarkById(userId: number, bookmarkId: number) {
+    return this.bookmarkRepository.findOne({
+      where: {
+        id: bookmarkId,
+        user: {
+          id: userId,
+        },
+      },
+    });
+  }
+
   public createBookmark(user: User, createBookmarkDto: CreateBookmarkDto) {
     const bookmark = this.bookmarkRepository.create({
       ...createBookmarkDto,
       user,
     });
     return this.bookmarkRepository.save(bookmark);
+  }
+
+  public async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    updateBookmarkDto: UpdateBookmarkDto,
+  ) {
+    const bookmark = await this.bookmarkRepository.findOne({
+      where: {
+        id: bookmarkId,
+      },
+      relations: {
+        user: true,
+      },
+    });
+    if (!bookmark || bookmark.user.id !== userId) {
+      throw new ForbiddenException('Access to resource denied');
+    }
+    this.bookmarkRepository.update(
+      {
+        id: bookmarkId,
+      },
+      updateBookmarkDto,
+    );
   }
 }
